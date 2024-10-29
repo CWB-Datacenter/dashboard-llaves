8ada35389731191bbf07280dbd26aaca4dfd0add
AbdielP <abdiel.pinzonc@gmail.com>
2024-10-29 15:04:46 -0500

    inner join de ubicaciones

--- src/components/Llaves/KeyResults.jsx
+++ src/components/Llaves/KeyResults.jsx
@@ -6,9 +6,12 @@ export const KeyResults = ({ searchTerm }) => {
   const { state } = useContext(DatacenterContext) 
   const { selectedDatacenter } = state
   const [keys, setKeys] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchKeys = async () => {
+      if (!selectedDatacenter) return
+      setLoading(true)
       try {
         const response = await fetch(`https://cwp-vidc-scat.cwpanama.com/llaves/api/selectLlaves.php?datacenter_id=${selectedDatacenter}`, {
           method: 'GET',
@@ -17,10 +20,13 @@ export const KeyResults = ({ searchTerm }) => {
           },
       })
       const data = await response.json()
+      console.log(data)
       setKeys(data)
       } catch (error) {
-        console.error("Error al obtener llaves:", error);
+        console.error("Error al obtener llaves:", error)
+        setKeys([])
       }
+      setLoading(false)
     }
     if (selectedDatacenter) fetchKeys()
   }, [selectedDatacenter])
@@ -31,15 +37,18 @@ export const KeyResults = ({ searchTerm }) => {
   )
 
   return (
-    <p>Como mostrar las llaves aquí...</p>
-    // <Box>
-    //   { filteredKeys.length > 0 ? (
-    //     filteredKeys.map(key => (
-    //       <Typography key={key.id}>{key.cliente_equipo} - {key.ubicacion}</Typography>
-    //     ))
-    //   ) : (
-    //     <Typography>No se encontraron llaves</Typography>
-    //   )}
-    // </Box>
+    <Box>
+      {loading ? (
+        <Typography>Cargando llaves...</Typography>
+      ) : searchTerm ? ( // Solo muestra resultados si hay un término de búsqueda
+        filteredKeys.length > 0 ? (
+          filteredKeys.map(key => (
+            <Typography key={key.id}>{key.codigo} - {key.cliente_equipo} - {key.nombre_ubicacion}</Typography>
+          ))
+        ) : (
+          <Typography>No se encontraron resultados</Typography>
+        )
+      ) : null} {/* No muestra nada si no hay búsqueda */}
+    </Box>
   )
 }
