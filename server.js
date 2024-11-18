aad5dbdcbed3b2952378a8a3d913f4a73ba9c384
AbdielP <abdiel.pinzonc@gmail.com>
2024-11-17 22:48:22 -0500

    archivo server para pruebas

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express();
+
+app.use(express.static(`./dist`));
+
+app.get(`/*`, function(req, res) {
+    res.sendFile(`index.html`, {root: `dist`});
+});
+
+app.listen(process.env.PORT || 80, function() {
+    console.log(`Listening: Server on port: `, process.env.PORT || 80);
+});
