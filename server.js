92170ab2aaf804bef4fb02096f5cc11a66d31dea
AbdielP <abdiel.pinzonc@gmail.com>
2024-11-18 15:41:58 -0500

    fuera ;

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-const app = express();
+const app = express()
 
-app.use(express.static(`./dist`));
+app.use(express.static(`./dist`))
 
 app.get(`/*`, function(req, res) {
-    res.sendFile(`index.html`, {root: `dist`});
-});
+    res.sendFile(`index.html`, {root: `dist`})
+})
 
 app.listen(process.env.PORT || 80, function() {
-    console.log(`Listening: Server on port: `, process.env.PORT || 80);
-});
+    console.log(`Listening: Server on port: `, process.env.PORT || 80)
+})
