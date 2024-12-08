a890cd8f2f9765c4f45ebb1e699374174e77392f
AbdielP <abdiel.pinzonc@gmail.com>
2024-12-08 00:52:00 -0500

    nombres dinamicos para las rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,26 @@
 import { Navbar } from '../components/Navbar'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { LlavesPage } from '../Pages/LlavesPage'
 import { QRCargaPage } from '../Pages/QRCargaPage'
-import { Box } from '@mui/material'
 
 export const AppRouter = () => {
+    const location = useLocation()
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/llaves':
+                document.title = 'Gestión de Llaves'
+                break
+            case '/qrcarga':
+                document.title = 'QR Carga'
+                break
+            default:
+                document.title = 'Gestión de llaves' // Título por defecto
+                break
+        }
+    }, [location])
+
     return (
         <>
             <Navbar />
